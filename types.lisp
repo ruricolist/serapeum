8f36a2fd054033e8a3e5f6042808005299821eeb
Paul M. Rodriguez <pmr@ruricolist.com>
2014-12-16 13:11:53 -0600

    Clarify the semantics of `assure` and add `assuref`.

--- types.lisp
+++ types.lisp
@@ -1,6 +1,6 @@
 (in-package :serapeum)
 
-(export '(-> assure wholenum))
+(export '(-> assure assuref wholenum))
 
 (deftype wholenum ()
   "A whole number. Equivalent to `(integer 0 *)'."
@@ -17,14 +17,63 @@
      (defun mod-fixnum+ (x y) ...)"
   `(declaim (ftype (-> ,args ,values) ,function)))
 
+(defun read-new-value ()
+  "Read and evaluate a value."
+  (format *query-io* "~&New value: ")
+  (list (eval (read *query-io*))))
+
+(defmacro wrong-type (datum type restart &body (report))
+  `(restart-case
+       (error 'type-error
+              :datum ,datum
+              :expected-type ,type)
+     (,restart (new)
+       :report ,report
+       :interactive read-new-value
+       new)))
+
+(defun require-type (datum spec)
+  (declare (optimize (debug 0)))
+  (if (typep datum spec)
+      datum
+      (let ((new (wrong-type datum spec use-value
+                   "Supply a value to use instead")))
+        (require-type new spec))))
+
+(defun require-type-for (datum spec place)
+  (declare (optimize (debug 0)))
+  (if (typep datum spec)
+      datum
+      (let ((new (wrong-type datum spec store-value
+                   (lambda (s) (format s "Supply a new value for ~s" place)))))
+        (require-type-for new spec place))))
+
 (defmacro assure (type-spec &body (form))
-  "Cross between CHECK-TYPE and THE for inline type checking.
-The syntax is the same as THE; the semantics are the same as
-CHECK-TYPE.
+  "Macro for inline type checking.
+
+`assure' is to `the' as `check-type' is to `declare'.
+
+     (the string 1) => undefined
+     (assure string 1) => error
+
+The value returned from the `assure' form is guaranteed to satisfy
+TYPE-SPEC. If FORM does not return a value of that type, then a
+correctable error is signaled. You can supply a value of the correct
+type with the `use-value' restart.
+
+Note that the supplied value is *not* saved into the place designated
+by FORM. (But see `assuref'.)
 
 From ISLISP."
+  `(the ,type-spec (require-type ,form ',type-spec)))
+
+(defmacro assuref (place type-spec)
+  "Like `(progn (check-type PLACE TYPE-SPEC) PLACE)`, but evaluates
+PLACE only once."
   (with-gensyms (temp)
-    `(the ,type-spec
-          (let ((,temp ,form))
-            (check-type ,temp ,type-spec)
-            ,temp))))
+    (let ((ts type-spec))
+      `(the ,ts
+            (let ((,temp ,place))
+              (if (typep ,temp ',ts)
+                  ,temp
+                  (setf ,place (require-type-for ,temp ',ts ',place))))))))

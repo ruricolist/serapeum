e8636316e6c08f8d14047c2f4145b2ddddfb1585
Paul M. Rodriguez <pmr@ruricolist.com>
2015-02-18 23:48:03 -0600

    Document defmethods.

--- clos.lisp
+++ clos.lisp
@@ -62,6 +62,69 @@ If X is a class, it designates itself."
 
 
 (defmacro defmethods (class (self . slots) &body body)
+  "Concisely define methods that specialize on the same class.
+
+You can use `defgeneric' to define methods on a single generic
+function without having to repeat the name of the function:
+
+    (defgeneric fn (x)
+      (:method ((x string)) ...)
+      (:method ((x number)) ...))
+
+Which is equivalent to:
+
+    (defgeneric fn (x))
+
+    (defmethod fn ((x string))
+      ...)
+
+    (defmethod fn ((x number))
+      ...)
+
+Similarly, you can use `defmethods' to define methods that specialize
+on the same class, and access the same slots, without having to
+repeat the names of the class or the slots:
+
+    (defmethods my-class (self x y)
+      (:method initialize-instance :after (self &key)
+        ...)
+      (:method print-object (self stream)
+        ...)
+      (:method some-method ((x string) self)
+        ...))
+
+Which is equivalent to:
+
+    (defmethod initialize-instance :after ((self my-class) &key)
+      (with-slots (x y) self
+        ...))
+
+    (defmethod print-object ((self my-class) stream)
+      (with-slots (x y) self
+        ...))
+
+    (defmethod some-method ((x string) (self my-class))
+      (with-slots (y) self              ;!
+        ...))
+
+Note in particular that `self' can appear in any position, and that
+you can freely specialize the other arguments.
+
+\(The difference from using `with-slots' is the scope of the slot
+bindings: they are established *outside* of the method definition,
+which means argument bindings shadow slot bindings:
+
+    (some-method \"foo\" (make 'my-class :x \"bar\"))
+    => \"foo\"
+
+Since slot bindings are lexically outside the argument bindings, this
+is surely correct, even if it makes `defmethods' slightly harder to
+explain in terms of simpler constructs.)
+
+Is `defmethods' trivial? Yes, in terms of its implementation. This
+docstring is far longer than the code it documents. But you may find
+it does a lot to keep heavily object-oriented code readable and
+organized, without any loss of power."
   `(macrolet ((:method (name &body body)
                 (let* ((class ',class)
                        (self ',self)
@@ -71,7 +134,7 @@ If X is a class, it designates itself."
                        (docstring (when (stringp (car body)) (pop body)))
                        (args-with-self (substitute (list self class) self args)))
                   (when (equal args-with-self args)
-                    (warn "No ~s binding in ~s" self args))
+                    (warn "No binding for ~s in ~s" self args))
                   `(symbol-macrolet ,(loop for slot in slots
                                            collect `(,slot (slot-value ,self ',slot)))
                      (defmethod ,name ,@(unsplice qualifier) ,args-with-self

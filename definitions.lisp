0c64e178e2d8e7bfdc9921690634affcff5a8c20
Paul M. Rodriguez <pmr@ruricolist.com>
2015-04-13 13:25:18 -0500

    Support internal definitions.

--- definitions.lisp
+++ definitions.lisp
@@ -5,7 +5,8 @@
           defalias
           def
           defplace
-          defcondition))
+          defcondition
+          local))
 
 ;;;# Lexical globals
 
@@ -161,3 +162,172 @@ Lisp."
   `(define-condition ,name ,supers
      ,slots
      ,@options))
+
+
+;; Internal definitions.
+
+(defmacro local (&body body &environment env)
+  "Make internal definitions using top-level definition forms.
+
+Within `local' you can use top-level definition forms and have them
+create purely local definitions, like `let', `labels', and `macrolet':
+
+     (fboundp 'plus) ; => nil
+
+     (local
+       (defun plus (x y)
+         (+ x y))
+       (plus 2 2))
+     ;; => 4
+
+     (fboundp 'plus) ; => nil
+
+Each form in BODY is subjected to partial expansion (with
+`macroexpand-1') until either it expands into a recognized definition
+form (like `defun') or it can be expanded no further.
+
+\(This means that you can use macros that expand into top-level
+definition forms to create local definitions.)
+
+Just as at the real top level, a form that expands into `progn' (or an
+equivalent `eval-when') is descended into, and definitions that occur
+within it are treated as top-level definitions.
+
+\(Support for `eval-when' is incomplete: `eval-when' is supported only
+when it is equivalent to `progn').
+
+The recognized definition forms are:
+
+- `def', for lexical variables (as with `letrec')
+- `defun', for local functions (as with `labels')
+- `defmacro', for local macros (as with `defmacro')
+- `defalias', to bind values in the function namespace (like `fbindrec*')
+- `declaim', to make declarations (as with `declare')
+- `define-symbol-macro', to bind symbol macros (as with `symbol-macrolet')
+- `defconstant' and `defconst', which behave exactly like symbol macros
+
+\(Note that the top-level definition forms defined by Common Lisp
+are (necessarily) supplemented by two from Serapeum: `def' and
+`defalias'.)
+
+The exact order in which the bindings are made depends on how `local'
+is implemented at the time you read this. The only guarantees are that
+variables are bound sequentially; functions can always close over the
+bindings of variables, and over other functions; and macros can be
+used once they are defined.
+
+     (local
+       (def x 1)
+       (def y (1+ x))
+       y)
+     => 2
+
+     (local
+       (defun adder (y)
+         (+ x y))
+       (def x 2)
+       (adder 1))
+     => 3
+
+Note that `let' forms are *not* descended into, so you cannot use the
+top-level idiom of wrapping `let' around `defun'.
+
+The value returned by the `local` form is that of the last form in
+BODY. Note that definitions have return values in `local' just like
+they do at the top level. For example:
+
+     (local
+       (plus 2 2)
+       (defun plus (x y)
+         (+ x y)))
+
+Returns `plus', not 4.
+
+The `local' macro is based on Racket's support for internal
+definitions (although not Racket's `local' macro, which does
+something different)."
+  ;; TODO Think about how to (whether to) handle dynamic vars?
+  (multiple-value-bind (body decls) (parse-body body)
+    (let (vars hoisted-vars labels macros symbol-macros)
+      (labels ((expand-partially (form)
+                 (if (consp form)
+                     (destructuring-case form
+                       ((declaim &rest specs)
+                        (dolist (spec specs)
+                          (push `(declare ,spec) decls)))
+                       ((def var expr &optional docstring)
+                        (declare (ignore docstring))
+                        ;; Remember `def' returns a symbol.
+                        (if (constantp expr env)
+                            (progn
+                              (push (list var expr) hoisted-vars)
+                              `',var)
+                            (progn (pushnew var vars)
+                                   `(progn (setf ,var ,expr) ',var))))
+                       ((defconstant name expr &optional docstring)
+                        (declare (ignore docstring))
+                        (push (list name expr) symbol-macros)
+                        `',name)
+                       ((defconst name expr &optional docstring)
+                        (declare (ignore docstring))
+                        (push (list name expr) symbol-macros)
+                        `',name)
+                       ((define-symbol-macro sym exp)
+                        (push (list sym exp) symbol-macros)
+                        `',sym)
+                       ((defun name args &body body)
+                        (push (list* name args body) labels)
+                        ;; `defun' returns a symbol.
+                        `',name)
+                       ((defalias name expr &optional docstring)
+                        (declare (ignore docstring))
+                        (let ((temp (gensym)))
+                          (push temp vars)
+                          (push `(,name (&rest args) (apply ,temp args)) labels)
+                          `(progn (setf ,temp ,expr) ',name)))
+                       ((defmacro name args &body body)
+                        (push (list* name args body) macros)
+                        ;; `defmacro' returns a symbol.
+                        `',name)
+                       ((progn &body body)
+                        `(progn ,@(mapcar #'expand-partially body)))
+                       ((eval-when situations &body body)
+                        (if (set-equal situations '(:load-toplevel :execute))
+                            (expand-partially `(progn ,@body))
+                            (error "Unsupported eval-when: ~a" situations)))
+                       ((otherwise &rest rest) (declare (ignore rest))
+                        (multiple-value-bind (exp exp?)
+                            (macroexpand-1 form env)
+                          (if exp?
+                              (expand-partially exp)
+                              form))))
+                     form)))
+        (let ((body (mapcar #'expand-partially body)))
+          (cond ((not (or vars labels macros symbol-macros))
+                 `(locally ,@decls ,@body))
+                ((no body)
+                 (error "Internal definitions are present, but there are ~
+              no expressions.~%Definitions: ~s"
+                        (append (mapcar #'second vars)
+                                (mapcar #'second labels)
+                                (mapcar #'second macros))))
+                (t
+                 (let* ((fn-names (mapcar (lambda (x) `(function ,(car x))) labels))
+                        (var-names (append (mapcar #'car hoisted-vars) vars)))
+                   (multiple-value-bind (var-decls decls)
+                       (partition-declarations var-names decls)
+                     (multiple-value-bind (fn-decls decls)
+                         (partition-declarations fn-names decls)
+                       `(locally ,@decls
+                          (symbol-macrolet ,symbol-macros
+                            (macrolet ,macros
+                              ;; As an optimization, hoist constant
+                              ;; bindings, e.g. (def x 1), so the compiler
+                              ;; can infer their types or make use of
+                              ;; declaration.
+                              (let (,@hoisted-vars
+                                    ,@vars)
+                                ,@var-decls
+                                (labels ,labels
+                                  ,@fn-decls
+                                  ,@body)))))))))))))))

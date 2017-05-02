5532fc4adefbcbf1c804e1847ac8e8bab6f27ddb
Paul M. Rodriguez <pmr@ruricolist.com>
2017-05-01 19:43:11 -0500

    Allow attaching names to locks.

--- threads.lisp
+++ threads.lisp
@@ -4,26 +4,33 @@
   (defconstant +lock-class+ (class-of (bt:make-lock))))
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
-  (defun lock-form (object objectp env)
+  (defun lock-form (object objectp env string)
     (cond ((not objectp)
-           `(load-time-value (bt:make-lock "Anonymous critical section")))
+           (let ((string (or string "Anonymous critical section")))
+             `(load-time-value (bt:make-lock ,string))))
           ((constantp object env)
-           `(load-time-value (monitor ,object)))
-          (t `(monitor ,object)))))
+           `(load-time-value
+             (monitor* ,object ,string)))
+          (t `(monitor* ,object ,string)))))
 
 (defmacro synchronized ((&optional (object nil objectp)) &body body &environment env)
   "Run BODY holding a unique lock associated with OBJECT.
-If no OBJECT is provided, run BODY as an anonymous critical section."
-  (let ((form (lock-form object objectp env)))
-    (with-gensyms (lock)
-      `(let ((,lock ,form))
-         (bt:with-lock-held (,lock)
-           ,@body)))))
+If no OBJECT is provided, run BODY as an anonymous critical section.
+
+If BODY begins with a literal string, attach the string to the lock
+object created (as the argument to `bt:make-lock')."
+  (multiple-value-bind (string? body)
+      (if (stringp (first body))
+          (values (first body) (rest body))
+          (values nil body))
+    (let* ((form (lock-form object objectp env string?)))
+      (with-gensyms (lock)
+        `(let ((,lock ,form))
+           (bt:with-lock-held (,lock)
+             ,@body))))))
 
 (defvar *monitors*
   (tg:make-weak-hash-table :weakness :key))
-
-(defvar *monitors-lock* (bt:make-lock "Monitor lock"))
 
 (defgeneric monitor (object)
   (:documentation "Return a unique lock associated with OBJECT."))
@@ -32,7 +39,16 @@ If no OBJECT is provided, run BODY as an anonymous critical section."
   object)
 
 (defmethod monitor ((object t))
-  ;; Clozure has lock-free hash tables.
-  (#+ccl progn #-ccl synchronized #-ccl ()
-    (ensure-gethash object *monitors*
-                    (bt:make-lock "Monitor"))))
+  nil)
+
+(defun monitor* (object string)
+  (or (monitor object)
+      (let ((string (or string "Monitor")))
+        (flet ((ensure-monitor (object string)
+                 (ensure-gethash object *monitors*
+                                 (bt:make-lock string))))
+
+          ;; Clozure has lock-free hash tables.
+          #+ccl (ensure-monitor object string)
+          #-ccl (synchronized ()
+                  (ensure-monitor object string))))))

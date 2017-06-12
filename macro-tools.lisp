c789f685e528547bbff8626b220ed5b38012d470
Paul M. Rodriguez <pmr@ruricolist.com>
2017-06-12 18:35:19 -0500

    Document define-case-macro.

--- macro-tools.lisp
+++ macro-tools.lisp
@@ -405,32 +405,84 @@ shooting yourself in the foot by unwittingly using a macro that calls
 ;;; Macro-writing macro for writing macros like `case'.
 
 (defmacro define-case-macro (name macro-args params &body macro-body)
-  "Write
+  "Define a macro like `case'.
 
-Beware: (NIL) becomes nil.
+A case-like macro is one that supports the following syntax:
 
-The default is just a list of forms.
-"
-  (multiple-value-bind (expr args clauses)
+- A list of keys is treated as matching any key in the list.
+- An empty list matches nothing.
+- The atoms T or `otherwise' introduce a default clause.
+- There can only be one default clause.
+- The default clause must come last.
+- Any atom besides the empty list, T, or `otherwise' matches itself.
+
+As a consequence of the above, to match against the empty list, T, or
+`otherwise', they must be wrapped in a list.
+
+    (case x
+      ((nil) \"Matched nil.\")
+      ((t) \"Matched t.\")
+      ((otherwise) \"Matched `otherwise'.\")
+      (otherwise \"Didn't match anything.\"))
+
+A macro defined using `define-case-macro' can ignore all of the above.
+It receives three arguments: the expression, already protected against
+multiple evaluation; a normalized list of clauses; and, optionally, a
+default clause.
+
+The clauses are normalized as a list of `(key . body)', where each key
+is an atom. (That includes nil, T, and `otherwise'.) Nonetheless, each
+body passed to the macro will only appear once in the expansion; there
+will be no duplicated code.
+
+The body of the default clause is passed separately,
+bound to the value of the `:default' keyword in PARAMS.
+
+    (define-case-macro my-case (expr &body clauses)
+        (:default default)
+      ....)
+
+Note that in this case, `default' will be bound to the clause's body
+-- a list of forms -- and not to the whole clause. The key of the
+default clause is discarded.
+
+If no binding is specified for the default clause, then no default
+clause is allowed.
+
+As a final example, if the `case' macro did not already exist, you
+could define it almost trivially using `define-case-macro':
+
+    (define-case-macro my-case (expr &body clause)
+        (:default default)
+      `(cond
+         ,@(loop for (key . body) in clauses
+                 collect `((eql ,expr ,key) ,@body))
+         (t ,@body)))"
+  (multiple-value-bind (expr other-args clauses)
       (ematch macro-args
         ((list expr '&body clauses)
          (values expr nil clauses))
-        ((list expr arg '&body clauses)
-         (values expr (list arg) clauses)))
-    (destructuring-bind (&key default
-                              (default-keys '(t otherwise)))
+        ((list expr other-arg '&body clauses)
+         (values expr (list other-arg) clauses)))
+    (destructuring-bind (&key
+                           error
+                           (default (and error (gensym)))
+                           (default-keys '(t otherwise)))
         params
       (let ((default-sym (or default (gensym))))
-        `(defmacro ,name (,expr ,@args &body ,clauses)
+        `(defmacro ,name (,expr ,@other-args &body ,clauses)
            (expand-case-macro
             (lambda (,expr ,default-sym ,clauses)
+              (declare (ignorable ,default-sym))
               ;; If `default' is defined as `nil', then no default
               ;; clause is allowed.
               ,(when (null default)
                  `(when ,default-sym
                     (error "Default disallowed in ~a" ,clauses)))
               ,@macro-body)
-            ,expr ,clauses :default-keys ',default-keys))))))
+            ,expr ,clauses
+            :default-keys ',default-keys
+            :error ',error))))))
 
 (defun clauses+default (clauses &key (default-keys '(t otherwise)))
   (let ((default-clause-tails
@@ -468,36 +520,46 @@ Otherwise, leave the keylist alone."
         else
           collect clause))
 
-(defun expand-case-macro (cont expr clauses &key (default-keys '(t otherwise)))
+(defun expand-case-macro (cont expr clauses &key (default-keys '(t otherwise)) error)
   (check-type clauses list)
+  (when (eql error t)
+    (setf error 'case-failure))
   (let ((cont
           (lambda (expr-temp default clauses)
             (assert (symbolp expr-temp))
             (assert (listp default))
             (assert (listp clauses))
-            (funcall cont expr-temp default clauses))))
-    (let ((expr-temp (gensym)))
-      ;; Rebind expr.
-      `(let ((,expr-temp ,expr))
-         ,(multiple-value-bind (clauses default)
-              (clauses+default clauses :default-keys default-keys)
-            (let* ((clauses (simplify-keylists clauses))
-                   (keys (mapcar #'first clauses)))
-              (if (every #'atom keys)   ;NB Nil could be a key.
-                  ;; Easy case. No lists of keys; do nothing special.
-                  (funcall cont expr-temp default clauses)
-                  ;; This could be done two ways: with flet or with
-                  ;; tagbody. I'm opting to use the version with flet
-                  ;; here, as I think it results in a more readable
-                  ;; expansion, and probably makes the job of type
-                  ;; inference easier. If it proves too expensive,
-                  ;; however, switching back to tagbody is
-                  ;; straightforward: just swap out
-                  ;; `expand-case-macro/flet' for
-                  ;; `expand-case-macro/tagbody'. (It might even be
-                  ;; worth using different expansions on different
-                  ;; Lisps.)
-                  (expand-case-macro/flet cont expr-temp clauses default))))))))
+            (funcall cont expr-temp default clauses)))
+        (expr-temp (gensym)))
+    ;; Rebind expr.
+    `(let ((,expr-temp ,expr))
+       ,(multiple-value-bind (clauses default)
+            (clauses+default clauses :default-keys default-keys)
+          (let* ((clauses (simplify-keylists clauses))
+                 (keys (mapcar #'first clauses))
+                 (clauses
+                   (or (and error
+                            (or default-keys
+                                (error "Cannot add an error clause without a default key."))
+                            (append clauses
+                                    (list `(,(random-elt default-keys)
+                                             (,error ,expr-temp ',keys)))))
+                       clauses)))
+            (if (every #'atom keys)     ;NB Nil could be a key.
+                ;; Easy case. No lists of keys; do nothing special.
+                (funcall cont expr-temp default clauses)
+                ;; This could be done two ways: with flet or with
+                ;; tagbody. I'm opting to use the version with flet
+                ;; here, as I think it results in a more readable
+                ;; expansion, and probably makes the job of type
+                ;; inference easier. If it proves too expensive,
+                ;; however, switching back to tagbody is
+                ;; straightforward: just swap out
+                ;; `expand-case-macro/flet' for
+                ;; `expand-case-macro/tagbody'. (It might even be
+                ;; worth using different expansions on different
+                ;; Lisps.)
+                (expand-case-macro/flet cont expr-temp clauses default)))))))
 
 (defun expand-case-macro/common (clauses &key jump)
   (check-type jump function)

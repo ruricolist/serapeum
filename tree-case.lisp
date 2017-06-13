e716c8dcd8b3e3e9850ccb2572c2b26300d7913a
Paul M. Rodriguez <pmr@ruricolist.com>
2017-06-12 19:21:56 -0500

    Simplify the partitioning.

--- tree-case.lisp
+++ tree-case.lisp
@@ -3,40 +3,33 @@
 ;; http://www.foldr.org/~michaelw/log/programming/lisp/icfp-contest-2006-vm
 (define-case-macro tree-case (keyform &body cases)
     (:default default)
-  "A variant of `case' optimized for when every key is an integer."
+  "A variant of `case' optimized for when every key is an integer.
+
+Comparison is done using `eql'."
   (let ((keys (mapcar #'first cases)))
     (dolist (key keys)
       (unless (typep key 'integer)
         (error 'type-error
                :datum key
                :expected-type 'integer))))
-
   (let ((cases (sort (copy-list cases) #'< :key #'first))
         (fail (string-gensym 'fail)))
     (labels ((rec (keyform cases)
-               (cond ((null cases)
-                      `(,fail))
-                     ((single cases)
-                      (destructuring-bind ((key . body)) cases
-                        `(if (= ,keyform ,key)
-                             (progn ,@body)
-                             (,fail))))
-                     (t (mvlet* ((left-cases right-cases (halves cases))
-                                 (split (caar right-cases)))
-                          `(if (< ,keyform ,split)
-                               ,(rec keyform left-cases)
-                               ,(rec keyform right-cases)))))))
-      `(flet ((,fail ()
-                ,@default))
+               (if (< (length cases) 4)
+                   `(case ,keyform
+                      ,@cases
+                      (t (,fail)))
+                   (mvlet* ((left-cases right-cases (halves cases))
+                            (split (caar right-cases)))
+                     `(if (< ,keyform ,split)
+                          ,(rec keyform left-cases)
+                          ,(rec keyform right-cases))))))
+      `(flet ((,fail () ,@default))
          ,(rec keyform cases)))))
 
-(define-case-macro tree-ecase (keyform nil &body clauses)
-    ()
+(define-case-macro tree-ecase (keyform &body clauses)
+    (:error t)
   "Like `tree-case', but signals an error if KEYFORM does not match
 any of the provided cases."
   `(tree-case ,keyform
-     ,@clauses
-     (otherwise
-      (error 'type-error
-             :datum ,keyform
-             :expected-type '(member ,@(mapcar #'first clauses))))))
+     ,@clauses))

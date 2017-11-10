2173dd6e5cb4f1553baa023bfc60aaaa6406a36f
Paul M. Rodriguez <pmr@ruricolist.com>
2017-11-09 18:28:08 -0600

    Make `synchronized` re-entrant.

--- threads.lisp
+++ threads.lisp
@@ -1,24 +1,35 @@
 (in-package #:serapeum)
 
+;;; NB We use to use non-recursive locks here, but on comparison with
+;;; other languages providing a `synchronized' keyword (Java,
+;;; Objective-C, C#, D) they all use a recursive lock, so that is what
+;;; we now use here.
+
 (eval-when (:compile-toplevel :load-toplevel)
-  (defconstant +lock-class+ (class-of (bt:make-lock))))
+  (defconstant +lock-class+ (class-of (bt:make-recursive-lock))))
+
+(defvar *monitors*
+  (tg:make-weak-hash-table
+   :weakness :key
+   ;; This should be plenty big enough to never need resizing.
+   :size 512))
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (defun lock-form (object objectp env string)
     (cond ((not objectp)
            (let ((string (or string "Anonymous critical section")))
-             `(load-time-value (bt:make-lock ,string))))
+             `(load-time-value (bt:make-recursive-lock ,string))))
           ((constantp object env)
            `(load-time-value
-             (monitor* ,object ,string)))
-          (t `(monitor* ,object ,string)))))
+             (ensure-monitor ,object ,string)))
+          (t `(ensure-monitor ,object ,string)))))
 
 (defmacro synchronized ((&optional (object nil objectp)) &body body &environment env)
   "Run BODY holding a unique lock associated with OBJECT.
 If no OBJECT is provided, run BODY as an anonymous critical section.
 
 If BODY begins with a literal string, attach the string to the lock
-object created (as the argument to `bt:make-lock')."
+object created (as the argument to `bt:make-recursive-lock')."
   (multiple-value-bind (string? body)
       (if (stringp (first body))
           (values (first body) (rest body))
@@ -26,11 +37,8 @@ object created (as the argument to `bt:make-lock')."
     (let* ((form (lock-form object objectp env string?)))
       (with-gensyms (lock)
         `(let ((,lock ,form))
-           (bt:with-lock-held (,lock)
+           (bt:with-recursive-lock-held (,lock)
              ,@body))))))
-
-(defvar *monitors*
-  (tg:make-weak-hash-table :weakness :key))
 
 (defgeneric monitor (object)
   (:documentation "Return a unique lock associated with OBJECT."))
@@ -41,14 +49,18 @@ object created (as the argument to `bt:make-lock')."
 (defmethod monitor ((object t))
   nil)
 
-(defun monitor* (object string)
+(defun ensure-monitor (object string)
   (or (monitor object)
       (let ((string (or string "Monitor")))
         (flet ((ensure-monitor (object string)
                  (ensure-gethash object *monitors*
-                                 (bt:make-lock string))))
-
+                                 (bt:make-recursive-lock string))))
           ;; Clozure has lock-free hash tables.
           #+ccl (ensure-monitor object string)
           #-ccl (synchronized ()
                   (ensure-monitor object string))))))
+
+(defclass synchronized ()
+  ((monitor :initform (bt:make-recursive-lock)
+            :reader monitor))
+  (:documentation "Mixin for a class with its own monitor."))

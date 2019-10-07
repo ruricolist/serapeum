9cc0f9c86b3fb8ac196cf7e82e7a3d826daaf55b
Paul M. Rodriguez <pmr@ruricolist.com>
2019-10-06 19:56:55 -0500

    Basic test suite for hooks.

--- tests/hooks.lisp
+++ tests/hooks.lisp
@@ -2,3 +2,54 @@
 
 (def-suite hooks :in serapeum)
 (in-suite hooks)
+
+(defun call/temp-hook (fn)
+  (let ((hook (gensym)))
+    (setf (symbol-value hook) nil)
+    (unwind-protect
+         (funcall fn hook)
+      (makunbound hook))))
+
+(defmacro with-temp-hook ((hook &key) &body body)
+  `(call/temp-hook
+    (lambda (,hook)
+      ,@body)))
+
+(test run-hook
+  (with-temp-hook (hook)
+    (let ((list '()))
+      (flet ((add-1 ()
+               (push 1 list))
+             (add-2 ()
+               (push 2 list))
+             (add-x (x)
+               (push x list)))
+        (add-hook hook #'add-2)
+        (add-hook hook #'add-1 :append t)
+        (run-hooks hook)
+        (is (equal list '(1 2)))))))
+
+(test run-hook-with-args ()
+  (with-temp-hook (hook)
+    (let ((list '()))
+      (flet ((add (n) (push n list)))
+        (add-hook hook #'add)
+        (run-hook-with-args hook '2)
+        (run-hook-with-args hook '1)
+        (is (equal list '(1 2)))))))
+
+(test run-hook-with-args-until-failure
+  (with-temp-hook (hook)
+    (add-hook hook (constantly nil))
+    (add-hook hook
+              (lambda () (fail "This function should not run"))
+              :append t)
+    (run-hook-with-args-until-failure hook)))
+
+(test run-hook-with-args-until-success
+  (with-temp-hook (hook)
+    (add-hook hook (constantly t))
+    (add-hook hook
+              (lambda () (fail "This function should not run"))
+              :append t)
+    (run-hook-with-args-until-success hook)))

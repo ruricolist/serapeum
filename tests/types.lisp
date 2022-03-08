57daf04efb938e6de168d6c41961a1e77f2c7613
Paul M. Rodriguez <pmr@ruricolist.com>
2022-03-08 16:44:55 -0600

    Always bind the test macro in with-test-fn

--- tests/types.lisp
+++ tests/types.lisp
@@ -170,3 +170,13 @@
   (is-false (typep '(1 . 2) '(soft-alist-of number number)))
   (is-false (typep '(1) '(soft-alist-of number number)))
   (is (typep '((x . y) (1 . 2) (3 . 4)) '(soft-alist-of t t))))
+
+(test (with-test-fn/high-safety :compile-at :run-time) ()
+  (is-true
+   (funcall (lambda (x y)
+              (declare (optimize (safety 3)
+                                 (speed 0)))
+              (let ((my-test #'eql))
+                (serapeum::with-test-fn (my-test)
+                  (my-test x y))))
+            1 1)))
